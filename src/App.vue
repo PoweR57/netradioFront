39c7fe77304ee60700ff216b04b8d17e943a2028
benier <benier2u@etu.univ-lorraine.fr>
2019-01-11 10:55:03 +0100

    affichage

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <!-- header -->
+    <div class="ui top attached menu">
+      <h1>Net'Radio</h1>
+      <div class="right menu">
+        <a class="item">Sign Up</a>
+      </div>
+    </div>
+    <!-- sous header -->
+    <div class="ui pointing menu">
+      <a class="active item">Home</a>
+      <a class="item">Replay</a>
+      <a class="item">Planning</a>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
+
 <script>
-export default {
-  name: 'App'
-}
+/* eslint-disable */
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1 {
+  margin-left: 45%;
+  font-size: 2rem;
 }
 </style>
+
