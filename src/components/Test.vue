9ddf227e17da190805a20b1775e79fff6d10a6e9
benier <benier2u@etu.univ-lorraine.fr>
2019-01-11 09:26:47 +0100

    test

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template><h1>Sa marche</h1> </template>
+
+
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+    
